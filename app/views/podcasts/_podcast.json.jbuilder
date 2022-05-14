c07c194d99bc1c83efb64987df4732e6d43f4a65
Martin Yanchev <myanch200@gmail.com>
2022-05-14 01:12:27 +0100

    generating scaffold for podcasts

---
+++ app/views/podcasts/_podcast.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! podcast, :id, :name, :descrption, :created_at, :updated_at
+json.url podcast_url(podcast, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :podcasts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "podcasts#index"
 end
